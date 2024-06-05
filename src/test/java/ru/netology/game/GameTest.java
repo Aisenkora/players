eff98a7faac5e8fd6015e375808823bcd575fa0d
Aesonne <radius.05-good@icloud.com>
2024-06-05 22:28:00 +0300

    Create project

---
+++ src/test/java/ru/netology/game/GameTest.java
@@ -0,0 +1,110 @@
+package ru.netology.game;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GameTest {
+
+    @Test
+    public void shouldRegisterPlayer() {
+        Game game = new Game();
+        Player player = new Player(4, "Aisenkora", 15);
+        game.register(player);
+
+        boolean expected = true;
+        boolean actual = game.isRegistered(player);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRoundWithRegisteredPlayers() throws NotRegisteredException {
+        Game game = new Game();
+        Player player1 = new Player(4, "Aisenkora", 100);
+        Player player2 = new Player(5, "Aesonne", 75);
+        game.register(player1);
+        game.register(player2);
+
+        game.round(player1.getName(), player2.getName());
+
+        int expected = 1;
+        int actual = game.round(player1.getName(), player2.getName());
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRoundIfSecondPlayerStronger() throws NotRegisteredException {
+        Game game = new Game();
+        Player player1 = new Player(4, "Aisenkora", 75);
+        Player player2 = new Player(5, "Aesonne", 100);
+        game.register(player1);
+        game.register(player2);
+
+        game.round(player2.getName(), player2.getName());
+
+        int expected = 2;
+        int actual = game.round(player1.getName(), player2.getName());
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRoundWithUnRegisteredPlayers() {
+        Game game = new Game();
+        Player player1 = new Player(4, "Aisenkora", 99);
+        Player player2 = new Player(5, "Aesonne", 75);
+        Exception exception = assertThrows(NotRegisteredException.class, () -> {
+            game.round("Неизвестный игрок", "Неизвестный игрок");
+        });
+
+        String expected = "Один из игроков не зарегистрирован";
+        String actial = exception.getMessage();
+        Assertions.assertEquals(expected, actial);
+    }
+
+    @Test
+    public void shouldRoundWithUnRegisteredPlayer1() {
+        Game game = new Game();
+        Player player1 = new Player(5, "Aesonne", 100);
+        Player player2 = new Player(4, "Aisenkora", 99);
+        game.register(player2);
+        Exception exception = assertThrows(NotRegisteredException.class, () -> {
+            game.round("Неизвестный игрок", "Aisenkora");
+        });
+
+        String expected = "Один из игроков не зарегистрирован";
+        String actial = exception.getMessage();
+        Assertions.assertEquals(expected, actial);
+    }
+
+    @Test
+    public void shouldRoundWithUnRegisteredPlayer2() {
+        Game game = new Game();
+        Player player1 = new Player(5, "Aesonne", 100);
+        Player player2 = new Player(4, "Aisenkora", 99);
+        game.register(player1);
+        Exception exception = assertThrows(NotRegisteredException.class, () -> {
+            game.round("Aesonne", "Неизвестный игрок");
+        });
+
+        String expected = "Один из игроков не зарегистрирован";
+        String actial = exception.getMessage();
+        Assertions.assertEquals(expected, actial);
+    }
+
+    @Test
+    public void shouldRoundWithTheSamePlayersStats() throws NotRegisteredException {
+        Game game = new Game();
+        Player player1 = new Player(4, "Aisenkora", 100);
+        Player player2 = new Player(5, "Aesonne", 100);
+
+        game.register(player1);
+        game.register(player2);
+
+        int excepted = 0;
+        int actual = game.round(player1.getName(), player2.getName());
+
+        Assertions.assertEquals(excepted, actual);
+    }
+
+}
